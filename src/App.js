529e7d449083eb8a1d1931850c0c9b80d5f7a270
filipe satiro <filipelipe202229@gmail.com>
2025-06-22 08:47:39 -0300

    pasta atualizada

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import SayMyName from './components/SayMayName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-
+import Evento from './components/Evento';
+import Form from './components/Forms';
 function App() {
-  const nome = "Maria";
+  
   return (
     <div className="App"> 
-    <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome = "Filipe"/>   
-      <SayMyName nome = "João"/>  
-      <SayMyName nome = {nome}/> 
-      <Pessoa nome="Oliveira" idade="30" profissao="Programador" foto="https://djairgalvao.com/wp-content/uploads/2021/08/pessoa.jpg"/>
+    <h1>Testando Eventos</h1>
+    <Evento numero="1"/>
+    <Evento numero="2"/>
+    <Form />
     </div>
   );
 }
