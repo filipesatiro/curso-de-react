9d054e21e5f25e7a571514e070133ee958a4eb1c
filipe satiro <filipelipe202229@gmail.com>
2025-06-23 09:27:30 -0300

    criado o formulário

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,16 +1,34 @@
+import { useState} from 'react';
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Usuário cadastrado com sucesso!");
+        console.log(`Usuário: ${name} foi cadastrado com a senha: ${password}`)
     }
+     const [name, setName] = useState()
+     const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="texto" placeholder="Digite o seu Nome"></input>
+                    <label htmlFor="name">Nome: </label>
+                    <input 
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Digite o seu Nome"
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha</label>
+                    <input type="password"
+                     placeholder="Digite a sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                     />
                 </div>
                 <div>
                     <input type="submit" value="cadastrar"/>
