ff3e5af314f9f50b7d0df410ead6e2defc83c085
filipe satiro <filipelipe202229@gmail.com>
2025-06-23 18:55:29 -0300

    pagina principal do react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import OutraLista from './components/OutraLista';
-
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import NavBar from './components/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
-  const meusItens = ['React', 'Vue', 'Angular'] 
   return (
-    <div className="App">
-    <h1> Renderização de Listas</h1>
-    <OutraLista itens={meusItens}/>
-    <OutraLista itens={[]}/>
-  
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 export default App;
